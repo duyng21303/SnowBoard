771d58f10299ce7dea8e91991fe6350f24a7d4c9
duyng21303 <duyng21303@gmail.com>
2025-02-22 03:15:27 +0700

    update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,124 +3,80 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // ▼ "Class/Member Variables" ▼
-    Rigidbody2D rb2d;
-    SurfaceEffector2D surfaceEffector2D;
+	Rigidbody2D rb2d;
+	SurfaceEffector2D surfaceEffector2D;
 
-
-    // ▼ "Serialize Field" 
-    //      → to Make it "Visible" in the "Inspector" ▼
-    [SerializeField] float torqueAmount = 1f;
-    [SerializeField] float boostSpeed = 30f; 
-    [SerializeField] float baseSpeed = 20f;
+	[SerializeField] float torqueAmount = 1f;
+	[SerializeField] float boostSpeed = 30f;
+	[SerializeField] float baseSpeed = 20f;
 	[SerializeField] float jumpForce = 10f;
 	bool isGrounded = true;
 	bool canMove = true;
 
+	void Start()
+	{
+		rb2d = GetComponent<Rigidbody2D>();
+		surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
+	}
 
+	void Update()
+	{
+		if (canMove)
+		{
+			RotatePlayer();
+			RespondToBoost();
+		}
 
-
-    // ▬ Start is called once before the first execution of Update after the MonoBehaviour is created  ▬
-    void Start()
-    {
-        // ▼ Get the "Rigid Body 2D" Component of the "Player" Object 
-        //      → and "Store it" in the "Local Variable" ▼
-        rb2d = GetComponent<Rigidbody2D>();
-
-        // ▼ Access the "FindObjectOfType()" Method ▼
-        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
-    }
-
-
-
-    // ▬ Update is called once per frame ▬
-    void Update()
-    {
-        // ▼ "If" the "Player" is "Can Move" ▼
-        if (canMove)
-        {
-            // ▼ "Call" the "Methods" ▼
-            RotatePlayer();
-            RespondToBoost(); // ◄◄ "Player Improvement" ◄◄
-        }
+		// Chỉ cho phép nhảy khi đang chạm đất
 		if (canMove && isGrounded && Input.GetKeyDown(KeyCode.Space))
 		{
 			Jump();
 		}
 	}
-        
 
+	public void DisableControls()
+	{
+		canMove = false;
+	}
 
-        
-    
-
-
-
-    // ▬ "DisableControls()" Method 
-    //       → (with a "public" Access Modifier)
-    //       → that "Disables" the "Input" of the "Player" ▬
-    public void DisableControls()
-    {
-        // ▼ Set "Can Move" to "False" ▼
-        canMove = false;
-    }
-
-
-
-
-	// ▬ "RotatePlayer()" Method ▬
 	void Jump()
 	{
-		// Xóa vận tốc theo trục Y trước khi nhảy để đảm bảo nhảy đồng đều
+		// Xóa vận tốc theo trục Y để đảm bảo nhảy đồng đều
 		rb2d.velocity = new Vector2(rb2d.velocity.x, 0f);
 		rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
 		isGrounded = false;
 	}
+
 	void RotatePlayer()
-    {
-        // ▼ "If" the "Left Arrow" Key is "Pressed" 
-        //      → the "Player" will "Rotate Left" ▼
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            // ▼ "Apply" a "Force" → to "Rotate Left" the "Player" 
-            //      → using "AddTorque()" Method ▼
-            rb2d.AddTorque(torqueAmount);
-        }
+	{
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			rb2d.AddTorque(torqueAmount);
+		}
+		else if (Input.GetKey(KeyCode.RightArrow))
+		{
+			rb2d.AddTorque(-torqueAmount);
+		}
+	}
 
+	void RespondToBoost()
+	{
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			surfaceEffector2D.speed = boostSpeed;
+		}
+		else
+		{
+			surfaceEffector2D.speed = baseSpeed;
+		}
+	}
 
-        // ▼ "Else If" the "Right Arrow" Key is "Pressed" 
-        //      → the "Player" will "Rotate Right" ▼
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            // ▼ "Apply" a "Force" → to "Rotate Right" the "Player" 
-            //      → using "AddTorque()" Method ▼
-            rb2d.AddTorque(-torqueAmount);
-        }
-    }
-
-
-
-     // ▬ "RespondToBoost()" Method 
-    //       → that "Increase" the "Player Speed" ▬
-    void RespondToBoost()
-    {
-       // ▼ "If" the "Up Arrow" Key is "Pressed" 
-        //      → the "Player" will "Boost/Increase Speed" ▼
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            // ▼ "Access" the "Surface Effector 2D" Component
-            //      → and "Change" It's "Speed" Property Value 
-            //      → to "Boost Speed" ▼
-            surfaceEffector2D.speed = boostSpeed;
-        }
- 
-        // ▼ Otherwise, "Stay" at "Normal Speed" ▼
-        else
-        {
-            // ▼ "Access" the "Surface Effector 2D" Component
-            //      → and "Change" It's "Speed" Property Value 
-            //      → to "Base Speed" ▼
-            surfaceEffector2D.speed = baseSpeed;
-        }       
-    }
+	// Phát hiện va chạm với đất để reset trạng thái "isGrounded"
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.CompareTag("Ground"))
+		{
+			isGrounded = true;
+		}
+	}
 }
